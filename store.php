1e39dcda5f9d8b4c07178ce91bca98fa81a55799
m-yuu <dokonanki@gmail.com>
2021-06-15 22:25:51 +0900

    修正

--- store.php
+++ store.php
@@ -4,6 +4,7 @@
 require_once('./Models/Task.php');
 
 
+
 // データの受け取り
 // スーパーグローバル変数
 $_POST['title'];
@@ -13,6 +14,7 @@ $_POST['contents'];
 $title = $_POST['title'];
 $contents = $_POST['contents'];
 $time = date("Y/m/d H:i:s");
+
 
 // DBへのデータ保存
 $task = new Task(); //インスタンス化
